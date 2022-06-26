fcc165182a6e99ce21635f5823d2c7ea28717f81
passLsk <471627950@qq.com>
2022-06-26 11:44:21 +0800

    路由配置完成

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,20 @@ import VueRouter from 'vue-router'
 Vue.use(VueRouter)
 const routes = [
   {
-    path: '/login', name: 'login', component: () => import('@/components/Login')
+    path: '/login', name: 'Login', component: () => import('@/views/Login')
+  },
+  {
+    path: '/',
+    name: 'Layout',
+    redirect: '/home',
+    component: () => import('@/views/Layout'),
+    children:
+      [
+        { path: '/home', name: 'Home', component: () => import('@/views/Home') },
+        { path: '/video', name: 'Video', component: () => import('@/views/Video') },
+        { path: '/question', name: 'Question', component: () => import('@/views/Question') },
+        { path: '/my', name: 'My', component: () => import('@/views/My') }
+      ]
   }
 ]
 const router = new VueRouter({
---
+++ src/views/Question/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>问答</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+</style>
