ebad69be74bf0181e430358a6f1d3d2b3ba064a3
passLsk <471627950@qq.com>
2022-06-26 09:52:20 +0800

    初始化

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './vant.js'
-import './styles/index.less'
 import 'amfe-flexible'
+import Vant from 'vant'
+import 'vant/lib/index.css'
+import './styles/index.less'
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
